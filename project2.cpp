710db37aba24d981e2f22dd8e719f2fff54b451e
Johnpaul-Nguyen <johnpaulwins727477@gmail.com>
2024-09-23 10:22:20 -0500

    resolved issues regarding input and output chips, display methods, and cleaning cout methods of debugging

--- project2.cpp
+++ project2.cpp
@@ -31,10 +31,11 @@ class Chip {
         Chip* getInput1();
         Chip* getInput2();
         int getInputValue(); // Returns input value
+        ~Chip();
 
 //****** OTHER METHODS AS NECESSARY ************//
 };
-Chip::Chip():chipType(' '), id(NULL), input1(NULL), input2(NULL), output(NULL), inputValue(0.0){};
+Chip::Chip():chipType(' '), id(" "), input1(NULL), input2(NULL), output(NULL), inputValue(0.0){};
 Chip::Chip(char type, const string& identification):chipType(type), id(identification), input1(NULL), input2(NULL), output(NULL), inputValue(0.0){};
 Chip::Chip(Chip* chip):chipType(chip->chipType), id(chip->id), input1(chip->input1), input2(chip->input2), output(chip->output), inputValue(chip->inputValue){};
 
@@ -46,35 +47,62 @@ void Chip::compute(){
 
 }
 
-void Chip::display() const{ // Chat const after display()
+void Chip::display() const{ // Chat const after display() // Chat: Make sure you are not displaying null pointers
     cout << "Start of Display function" << endl;
     switch (chipType)
     {
     case 'I':
         cout << "Case 'I'" << endl;
-        cout << chipType << id << ", Output = " << output->getType() << output->getId() << endl;
+        if (output == NULL){
+            cout << "Output is none" << endl;
+        }
+        else{
+            cout << chipType << id << ", Output = " << output->getType() << output->getId() << endl;
+        }
+        
         break;
     case 'O':
         cout << "Case 'O'" << endl;
-        cout << chipType << id << "Input 1 = " << input1->getType() << input1->getId() << endl;
+        if (input1 == NULL){
+            cout << chipType << id << ", Input is none" << endl;
+        }
+        else{
+            cout << chipType << id << ", Input 1 = " << input1->getType() << input1->getId() << endl;
+        }
+
+        
         break;
     default:
         cout << "Default Case" << endl;
+        string in1;
         string in2;
-        bool none = false;
-
-        if (input2 == NULL){ // Check to see if chip has a second input
-            in2 = "None";
-            none = true;
-        }
+        bool none1 = (input1 == NULL);
+        bool none2 = (input2 == NULL);
 
         // Display chip info of chiptype, id, inputchip1 info, inputchip2 info, and outputchip info
-        if (none){
-            cout << chipType << id << "Input 1 = " << input1->getType() << input1->getId() << ", Input 2 = " << in2 << ", Output = " << output->getType() << output->getId() << endl;
+        if (none2 && none1){
+            in1 = "None";
+            in2 = "None";
+
+            cout << chipType << id << ", Input 1 = " << in1 << ", Input 2 = " << in2 << ", Output = " << output->getType() << output->getId() << endl;
+        }
+        else if(none1){
+            in1 = "None";
+            in2 = input2->getType() + input2->getId();
+
+            cout << chipType << id << ", Input 1 = " << in1 << ", Input 2 = " << in2 << ", Output = " << output->getType() << output->getId() << endl;
+        }
+        else if(none2){
+            in1 = input1->getType() + input1->getId();
+            in2 = "None";
+
+            cout << chipType << id << ", Input 1 = " << in1 << ", Input 2 = " << in2 << ", Output = " << output->getType() << output->getId() << endl;
         }
         else{
-            cout << chipType << id << "Input 1 = " << input1->getType() << input1->getId() << ", Input 2 = " << input2->getType() << input2->getId() << 
-            ", Output = " << output->getType() << output->getId() << endl;
+            in1 = input1->getType() + input1->getId();
+            in2 = input2->getType() + input2->getId();
+
+            cout << chipType << id << ", Input 1 = " << in1 << ", Input 2 = " << in2 << ", Output = " << output->getType() << output->getId() << endl;
         }
         break;
     }
@@ -85,6 +113,10 @@ char Chip::getType(){ return chipType; }
 int Chip::getInputValue(){ return inputValue; }
 Chip* Chip::getInput1(){ return input1; }
 Chip* Chip::getInput2(){ return input2; }
+
+Chip::~Chip(){
+    
+}
 
 int main(){
     cout << "Start Main" << endl;
@@ -101,11 +133,9 @@ int main(){
         //read the chip ID based on the first letter to determine its type
         string input;
         cin >> input;
-        cout << "Input: " << input << endl;
-
+        
         char chipType = input.at(0);
         string chipId = input.substr(1, input.size());
-        cout << "First Char: " << chipType << " chipId: " << chipId << endl;
         //create the chip object and initialize it appropriately
         Chip* chip = new Chip(chipType, chipId);
 
@@ -113,17 +143,13 @@ int main(){
         allChips[i] = chip;
     }
 
-    cout << "Num Chips: " << numChips << endl;
-
     for (int i = 0; i < numChips; i++){
         cout << "Chip type: " << allChips[i][0].getType() << ", Chip id: " << allChips[i][0].getId() << endl;
     }
 
     cin >> numCommands;
-    cout << "Number of commands: " << numCommands << endl;
 
     for (int i = 0; i < numCommands; i++) {
-        cout << "Iteration: " << i << endl;
 
         // read from input the links and make the appropriate
         //connections. You may need to search the array allChips
@@ -134,29 +160,36 @@ int main(){
         string command;
         string firstInput;
         string secondInput;
+        int secondInputInt;
+        Chip* firstChip;
+        Chip* secondChip;
+        int firstChipIndex = 0;
+        int secondChipIndex = 0;
+        
         cin >> command;
-
-        cout << "Command: " << command << endl;;
 
         /* Note to self: Think about making a function that reads in the circuit type and id to easily store 
         inputs and outputs*/
-
+        /* I need to find a way to assign outputs of chips. You have inputs already, How do you assign outputs?*/
 
         switch (command.at(0))
         {
         case 'I':
             cout << "Case I" << endl;
-            cin >> firstInput;
-            cout << "First Input: " << firstInput << endl;
+            cin >> firstInput >> secondInputInt;
+            cout << "First Input: " << firstInput << ", Value of input: " << secondInputInt << endl;
 
-            
+            for (int j = 0; j < numChips; j++){
+                if (allChips[j][0].getType() == firstInput.at(0) && allChips[j][0].getId() == firstInput.substr(1, firstInput.size())){
+                    allChips[j][0].setInputValue(secondInputInt);
+                }
+            }
 
             break;
 
         case 'O':
             cout << "Case O" << endl;
             cin >> firstInput;
-            cout << "First Input: " << firstInput;
 
             int outIndex;
             for (int j = 0; j < numChips; j++){
@@ -174,21 +207,14 @@ int main(){
             cout << "Default Case" << endl;
 
             cin >> firstInput >> secondInput;
-            cout << "First Input: " << firstInput << ", Second Input: " << secondInput << endl;
 
-            Chip* firstChip;
-            Chip* secondChip;
-            int firstChipIndex = -1;
-            int secondChipIndex = -1;
-
-            cout << "Before For Loop" << endl;
             for (int j = 0; j < numChips; j++){
                 if (allChips[j][0].getType() == firstInput.at(0) && allChips[j][0].getId() == firstInput.substr(1, firstInput.size())){
                     firstChip = allChips[j];
                     firstChipIndex = j;
                 }
                 if (allChips[j][0].getType() == secondInput.at(0) && allChips[j][0].getId() == secondInput.substr(1, secondInput.size())){
-                    secondChip[j];
+                    secondChip = allChips[j];
                     secondChipIndex = j;
                 }
                 
@@ -196,31 +222,30 @@ int main(){
                     break;
                 }
             }
-            cout << "After For Loop" << endl;
-           
-            cout << "Before if" << endl;
 
             if (allChips[secondChipIndex][0].getInput1() == NULL){
-                cout << "Input into first" << endl;
-                allChips[secondChipIndex][0].setInput1(firstChip);
+                allChips[secondChipIndex][0].setInput1(allChips[firstChipIndex]);
+                
             }
-            else{
-                cout << "Input into second" << endl;
-                allChips[secondChipIndex][0].setInput2(firstChip);
+            else if(allChips[secondChipIndex][0].getInput2() == NULL){
+                allChips[secondChipIndex][0].setInput2(allChips[firstChipIndex]);
             }
-            cout << "after if" << endl;
+
+            allChips[firstChipIndex][0].setOutput(allChips[secondChipIndex]);
 
             break;
         }
     }
 
-    for (int i = 0; i < numChips; i++){
-        cout << "Chip: " << allChips[i][0].getType() << allChips[i][0].getId() << ", Input1: " << allChips[i][0].getInput1() << ", Input2: " << allChips[i][0].getInput2() << endl;
-    }
     
     cout << "***** Showing the connections that were established" << endl;
     //for each component created call the display () method
     
+    for (int i = 0; i < numChips; i++){
+        cout << "Iteration of displaying: " << i << endl;
+        
+        allChips[i]->display();
+    }
     
     return 0;
 
